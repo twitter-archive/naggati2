75455644287e2b0e967567b020b669cc34f12916
Robey Pointer <robey@twitter.com>
2010-11-16 20:45:53 -0800

    disable cross-paths again, and use a newer netty.

--- project/build/NaggatiProject.scala
+++ project/build/NaggatiProject.scala
@@ -2,7 +2,7 @@ import sbt._
 import com.twitter.sbt._
 
 class NaggatiProject(info: ProjectInfo) extends StandardProject(info) with DefaultRepos {
-  val netty = "org.jboss.netty" % "netty" % "3.2.2"
+  val netty = "org.jboss.netty" % "netty" % "3.2.3.Final"
 
   // scala actors library with fork-join replaced by java 5 util.concurrent:
   // FIXME: we should investigate akka actors.
@@ -15,6 +15,4 @@ class NaggatiProject(info: ProjectInfo) extends StandardProject(info) with Defau
   val cglib = "cglib" % "cglib" % "2.1_3" % "test"
   val asm = "asm" % "asm" % "1.5.3" % "test"
   val objenesis = "org.objenesis" % "objenesis" % "1.1" % "test"
-
-  override def disableCrossPaths = false
 }
