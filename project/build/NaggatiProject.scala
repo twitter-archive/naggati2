608c8adebe69b7cb009bb9c9c344be240ef78b15
Stephen Judkins <stephen.judkins@gmail.com>
2010-12-20 20:24:03 -0800

    Updated dependencies to Scala 2.8.1 and Specs 1.6.6

--- project/build/NaggatiProject.scala
+++ project/build/NaggatiProject.scala
@@ -9,7 +9,7 @@ class NaggatiProject(info: ProjectInfo) extends StandardProject(info) with Defau
   val twitter_actors = "com.twitter" % "twitteractors_2.8.0" % "2.0.1"
 
   // for tests:
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
   val jmock = "org.jmock" % "jmock" % "2.4.0" % "test"
   val hamcrest_all = "org.hamcrest" % "hamcrest-all" % "1.1" % "test"
   val cglib = "cglib" % "cglib" % "2.1_3" % "test"
