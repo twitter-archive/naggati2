d1c68e8ba37926203779f22039fa4266d1511d82
Stephan Zuercher <stephan@twitter.com>
2012-09-26 10:38:00 -0700

    'release commit for com.twitter:naggati:4.0.1'

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object Naggati extends Build {
   ).settings(
     name := "naggati",
     organization := "com.twitter",
-    version := "4.0.1-SNAPSHOT",
+    version := "4.0.1",
     scalaVersion := "2.9.2",
 
     libraryDependencies ++= Seq(
